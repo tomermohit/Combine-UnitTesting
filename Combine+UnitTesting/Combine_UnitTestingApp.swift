b5f573bdd497dcd9063a1bc6ecee84e9ec1e55ab
tomermohit <mstomer31@gmail.com>
2024-03-25 21:08:46 +0530

    Initial Commit

---
+++ Combine+UnitTesting/Combine_UnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_UnitTestingApp.swift
+//  Combine+UnitTesting
+//
+//  Created by ꧁☆༒☬MoʜɩtToɱɘʀ☬༒☆꧂ on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Combine_UnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
